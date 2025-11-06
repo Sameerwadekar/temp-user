644e328e730eef6faa95054ff80bc0e5a1282d7c
Sameerwadekar <snehalsameer2005@gmail.com>
2025-11-06 16:15:26 +0530

    database and spring boot implementation

---
+++ temp-backend/src/test/java/com/learn/temp_backend/TempBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.temp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
