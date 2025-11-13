de884fec3e4a257a85fa5dc20e932cbd1d40e0ed
Sameerwadekar <snehalsameer2005@gmail.com>
2025-11-13 21:41:14 +0530

    adding & getting products

--- temp-backend/src/main/java/com/learn/temp_backend/service/ProductServiceImpl.java
+++ temp-backend/src/main/java/com/learn/temp_backend/service/ProductServiceImpl.java
@@ -1,27 +1,37 @@
 package com.learn.temp_backend.service;
 
 import java.util.List;
-
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.learn.temp_backend.dtos.ProductDto;
+import com.learn.temp_backend.entities.Product;
 import com.learn.temp_backend.repositary.ProductRepositary;
 
 @Service
 public class ProductServiceImpl implements ProductService{
+
+    private final ModelMapper modelMapper;
 	@Autowired
 	private ProductRepositary productRepositary;
+
+    ProductServiceImpl(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
 	@Override
 	public ProductDto addProduct(ProductDto productDto) {
-		// TODO Auto-generated method stub
-		return null;
+		Product product = modelMapper.map(productDto, Product.class);
+		Product saved = productRepositary.save(product);
+		ProductDto savedDto = modelMapper.map(saved, ProductDto.class);
+		return savedDto;
 	}
 
 	@Override
 	public List<ProductDto> getAllProducts() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Product> products = productRepositary.findAll();
+		List<ProductDto> list = products.stream().map((e)->modelMapper.map(e, ProductDto.class)).toList();
+		return list;
 	}
 
 	@Override
