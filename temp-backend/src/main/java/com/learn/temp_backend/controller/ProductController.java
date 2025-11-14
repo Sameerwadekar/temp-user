3f73246cc6be13b6b4db8f06b7f1ad4c896d6940
Sameerwadekar <snehalsameer2005@gmail.com>
2025-11-14 14:56:07 +0530

    added category

--- temp-backend/src/main/java/com/learn/temp_backend/controller/ProductController.java
+++ temp-backend/src/main/java/com/learn/temp_backend/controller/ProductController.java
@@ -7,22 +7,31 @@ import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learn.temp_backend.dtos.ProductDto;
+import com.learn.temp_backend.repositary.ProductRepositary;
 import com.learn.temp_backend.service.ProductService;
-
 
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    private final ProductRepositary productRepositary;
 	@Autowired
 	private ProductService productService;
+
+    ProductController(ProductRepositary productRepositary) {
+        this.productRepositary = productRepositary;
+    }
 	
 	@PostMapping
 	public ResponseEntity<ProductDto> addProduct(@RequestBody ProductDto productDto){
@@ -33,5 +42,25 @@ public class ProductController {
 	public ResponseEntity<List<ProductDto>> allProducts(){
 		List<ProductDto> allProducts = productService.getAllProducts();
 		return new ResponseEntity<List<ProductDto>>(allProducts,HttpStatus.OK);
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<ProductDto> getProductById(@PathVariable int id){
+		ProductDto productById = productService.getProductById(id);
+		return new ResponseEntity<ProductDto>(productById,HttpStatus.OK);
+	}
+	@PutMapping("/{id}")
+	public ResponseEntity<ProductDto> updateProduct(@PathVariable int id,@RequestBody ProductDto productDto){
+		ProductDto updatedProductbyId = productService.updateProductbyId(id, productDto);
+		return new ResponseEntity<ProductDto>(updatedProductbyId, HttpStatus.OK);
+	}
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteProduct(@PathVariable int id){
+		productService.deleteProduct(id);
+		return new ResponseEntity<String>("Product Deleted",HttpStatus.OK);
+	}
+	@PutMapping("/{pid}/category/{cid}")
+	public ResponseEntity<ProductDto> assignCategoty(@PathVariable int pid, @PathVariable int cid){
+		ProductDto productDto = productService.assignCategory(pid, cid);
+		return new ResponseEntity<ProductDto>(productDto,HttpStatus.OK);
 	}
 }
--- temp-backend/src/main/java/com/learn/temp_backend/service/ProductServiceImpl.java
+++ temp-backend/src/main/java/com/learn/temp_backend/service/ProductServiceImpl.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.learn.temp_backend.dtos.ProductDto;
+import com.learn.temp_backend.entities.Category;
 import com.learn.temp_backend.entities.Product;
+import com.learn.temp_backend.repositary.CategoryRepositary;
 import com.learn.temp_backend.repositary.ProductRepositary;
 
 @Service
@@ -15,13 +17,18 @@ public class ProductServiceImpl implements ProductService{
     private final ModelMapper modelMapper;
 	@Autowired
 	private ProductRepositary productRepositary;
+	
+	@Autowired
+	private CategoryRepositary categoryRepositary;
 
     ProductServiceImpl(ModelMapper modelMapper) {
         this.modelMapper = modelMapper;
     }
 	@Override
 	public ProductDto addProduct(ProductDto productDto) {
+		Category category = categoryRepositary.findById(productDto.getCategoryId()).orElseThrow(()->new RuntimeException("category id not found"));
 		Product product = modelMapper.map(productDto, Product.class);
+		product.setCategory(category);
 		Product saved = productRepositary.save(product);
 		ProductDto savedDto = modelMapper.map(saved, ProductDto.class);
 		return savedDto;
@@ -36,20 +43,38 @@ public class ProductServiceImpl implements ProductService{
 
 	@Override
 	public ProductDto getProductById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Product product = productRepositary.findById(id).orElseThrow(()-> new RuntimeException("Id not found"));
+		ProductDto productDto = modelMapper.map(product, ProductDto.class);
+		return productDto;
 	}
 
 	@Override
 	public ProductDto updateProductbyId(int id, ProductDto productDto) {
-		// TODO Auto-generated method stub
-		return null;
+		Product product = productRepositary.findById(id).orElseThrow(()->new RuntimeException("id not found"));
+		Category category = categoryRepositary.findById(productDto.getCategoryId()).orElseThrow(()->new RuntimeException("category id not found"));
+		product.setDescription(productDto.getDescription());
+		product.setName(productDto.getName());
+		product.setPrice(productDto.getPrice());
+		product.setCategory(category);
+		Product savedproduct = productRepositary.save(product);
+		ProductDto savedDto = modelMapper.map(savedproduct, ProductDto.class);	
+		return savedDto;
 	}
 
 	@Override
 	public void deleteProduct(int id) {
-		// TODO Auto-generated method stub
-		
+		Product product = productRepositary.findById(id).orElseThrow(()-> new RuntimeException("id not found"));
+		int pid = product.getId();
+		productRepositary.deleteById(pid);
+	}
+	@Override
+	public ProductDto assignCategory(int pid, int cid) {
+		Product product = productRepositary.findById(pid).orElseThrow(()-> new RuntimeException("id not found"));
+		Category category = categoryRepositary.findById(cid).orElseThrow(()-> new RuntimeException("id not found"));
+		product.setCategory(category);
+		Product savedProduct = productRepositary.save(product);
+		ProductDto productDto = modelMapper.map(savedProduct, ProductDto.class);		
+		return productDto;
 	}
 
 }
