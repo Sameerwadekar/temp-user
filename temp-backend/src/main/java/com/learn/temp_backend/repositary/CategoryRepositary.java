5c91ba9eb7a2bee82575c6a8059cc573dfe0f114
Sameerwadekar <snehalsameer2005@gmail.com>
2025-11-22 00:15:22 +0530

    added toggle stock availibility

--- temp-backend/src/main/java/com/learn/temp_backend/repositary/CategoryRepositary.java
+++ temp-backend/src/main/java/com/learn/temp_backend/repositary/CategoryRepositary.java
@@ -1,11 +1,14 @@
 package com.learn.temp_backend.repositary;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.learn.temp_backend.entities.Category;
+import com.learn.temp_backend.entities.Product;
 
 @CrossOrigin(origins = "http://localhost:5173/")
 public interface CategoryRepositary extends JpaRepository<Category, Integer> {
-
+//	List<Product> findByIdAndAvailableTrue();
 }
--- temp-backend/src/main/java/com/learn/temp_backend/repositary/ProductRepositary.java
+++ temp-backend/src/main/java/com/learn/temp_backend/repositary/ProductRepositary.java
@@ -1,9 +1,14 @@
 package com.learn.temp_backend.repositary;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.learn.temp_backend.entities.Product;
 
+@RepositoryRestResource(path = "product")
 public interface ProductRepositary extends JpaRepository<Product, Integer>{
 
+		List<Product> findByAvailableTrue();
 }
