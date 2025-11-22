0a977af15659d3ddc8553fe8960b5aa4e0978f7d
Sameerwadekar <snehalsameer2005@gmail.com>
2025-11-22 16:52:55 +0530

    admin login products & orders ui

---
+++ temp-admin/src/components/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DataTableDemo from './product-sheet'
+
+function Product() {
+  return (
+    <div>
+      <DataTableDemo/>
+    </div>
+  )
+}
+
+export default Product
---
+++ temp-admin/src/components/common/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SheetWithNavigationMenuDemo from './navigation-menu'
+
+function Navbar() {
+  return (
+    <div className='fixed top-4 right-4 z-50'>
+      <SheetWithNavigationMenuDemo/>
+    </div>
+  )
+}
+
+export default Navbar
