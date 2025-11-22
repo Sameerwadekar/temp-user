febe0e0d2a2cef3dc35feeb36759417438fae564
Sameerwadekar <snehalsameer2005@gmail.com>
2025-11-22 13:27:33 +0530

    adminsetup ui

---
+++ temp-admin/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+import { Input } from './components/ui/input'
+
+
+
+function App() {
+
+
+  return (
+    <>
+    <Button className="border border-amber-200">Hello</Button>
+    <Input/>
+
+    </>
+  )
+}
+
+export default App
