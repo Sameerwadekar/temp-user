a86dc154e6c1e309c836116dcf90b5d1dc0b1c1e
Sameerwadekar <snehalsameer2005@gmail.com>
2025-11-29 00:36:08 +0530

    jwt implementation

--- temp-backend/src/main/java/com/learn/temp_backend/repositary/UserRepositary.java
+++ temp-backend/src/main/java/com/learn/temp_backend/repositary/UserRepositary.java
@@ -3,7 +3,11 @@ package com.learn.temp_backend.repositary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.learn.temp_backend.entities.User;
+import java.util.Optional;
+
 
 public interface UserRepositary extends JpaRepository<User, String> {
-
+	Optional<User> findByEmail(String email);
+	boolean existsByEmail(String email);
+	
 }
